a75ecf64d3c7b174932eeef2cb3bfb1c0d0bbc5d
ahujamonica <ahujamonica9321@gmail.com>
2024-08-15 19:28:28 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greet();  // New method added
+    }
+
+    public static void greet() {
+        System.out.println("Welcome to version control with Git!");
     }
 }
 
